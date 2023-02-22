a0c6611b4b63f2d07f0c510b3f0b1af5fc5baa39
Luis Ardila <luisardilag@gmail.com>
2023-02-22 16:57:37 -0300

    update: agregado tailwind y otros config

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Pagina de Nosotros
+@endsection
+
+
+@section('contenido')
+    Contenido sobre nosotros
+@endsection
---
+++ resources/views/tienda.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Página de la Tienda
+@endsection
+
+
+@section('contenido')
+    Contenido a vender en la tienda
+@endsection
